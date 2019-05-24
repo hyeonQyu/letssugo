36d76725fa35f59612d25091c604d016b4e35c39
hyeonQyu <dhk0561@naver.com>
2019-05-25 00:50:59 +0900

    Make demo-animator for SSung-Ma Making building point UI

---
+++ Vuforia AR/Assets/Scripts/SSungMaController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SSungMaController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _ssungMa;
+    private bool _isWalk;
+
+    public void OnClickSSungMa()
+    {
+        Animator animator = _ssungMa.GetComponent<Animator>();
+        if(animator != null)
+        {
+            _isWalk = !_isWalk;
+            animator.SetBool("Walk", _isWalk);
+        }
+    }
+}
