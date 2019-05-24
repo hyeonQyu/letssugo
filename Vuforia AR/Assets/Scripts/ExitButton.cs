1418c081dc380ac6fb2054200e3efb13963c426a
hyeonQyu <dhk0561@naver.com>
2019-05-24 21:20:50 +0900

    Making building information UI

---
+++ Vuforia AR/Assets/Scripts/ExitButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _panel;
+    
+    public void OnClickExitButton()
+    {
+        Animator animator = _panel.GetComponent<Animator>();
+        if(animator != null)
+            animator.SetBool("open", false);
+    }
+}
---
+++ Vuforia AR/Assets/Scripts/OpenBuildingInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenBuildingInfo : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _panel;
+
+    public void OnClickOpenButton()
+    {
+        Animator animator = _panel.GetComponent<Animator>();
+        if(animator != null)
+            animator.SetBool("open", true);
+    }
+}
