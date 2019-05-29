c3d5396247d15899f0e064d403f025273c9ee24c
hyeonQyu <dhk0561@naver.com>
2019-05-29 16:34:50 +0900

    Remove altitude

--- Vuforia AR/Assets/Scripts/OpenBuildingInfo.cs
+++ Vuforia AR/Assets/Scripts/OpenBuildingInfo.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OpenBuildingInfo : MonoBehaviour
 {
     [SerializeField]
     private GameObject _panel;
+    [SerializeField]
+    private Image _imgBuilding;
+    [SerializeField]
+    private Image _imgName;
 
     public void OnClickOpenButton()
     {
         Animator animator = _panel.GetComponent<Animator>();
+
         if(animator != null)
+        {
+            //_imgBuilding.sprite = Resources.Load<Sprite>("UI/Buildings/" + name);
+            _imgName.sprite = Resources.Load<Sprite>("UI/Building Names/" + name + "UI");
+
             animator.SetBool("open", true);
+        }
     }
 }
